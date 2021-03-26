5efc9a994a9cd540069e171270cf0d05ccb1a286
YAZID <72652199+Yazid-ysr14@users.noreply.github.com>
2021-03-26 20:20:54 +0700

    Add files via upload

---
+++ check_host.py
@@ -0,0 +1,6 @@
+subprocess
+p1 = subprocess.Popen([“ping”,"IP","address"], stdout=subprocess.PIPE)
+p2 = subprocess.Popen([“echo”, “Gagal: IP address belum diberikan”], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)
